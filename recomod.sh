0ec181311c0182018fb619e2491d97808c59bbe0
CoolElectronics <58010778+CoolElectronics@users.noreply.github.com>
2023-04-23 22:48:20 -0400

    allow automatic resizing of the --stripped binary

--- recomod.sh
+++ recomod.sh
@@ -19,6 +19,22 @@ debug() {
     echo -e >&2 "\x1B[33mDebug: $*\x1b[39;49m"
   fi
 }
+
+supress(){
+  if [ "$FLAGS_debug" = "$FLAGS_TRUE" ] && [ "$FLAGS_quiet" = "$FLAGS_FALSE" ]; then
+    $@
+  else
+     >/dev/null 2>&1 $@
+  fi
+}
+suppress_err(){
+  if [ "$FLAGS_debug" = "$FLAGS_TRUE" ] && [ "$FLAGS_quiet" = "$FLAGS_FALSE" ]; then
+    $@
+  else
+     2> /dev/null $@
+  fi
+}
+
 info() {
   if [ "$FLAGS_quiet" = "$FLAGS_FALSE" ]; then
     echo -e "\x1B[32mInfo: $*\x1b[39;49m"
@@ -28,7 +44,7 @@ info() {
 traps() {
   set -e
   trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-  trap 'echo "\"${last_command}\" command failed with exit code $?. THIS IS A BUG, REPORT IT HERE https://github.com/MercuryWorkshop/fakemurk"' EXIT
+  trap 'echo "\"${last_command}\": "$BASH_COMMAND" command failed with exit code $?. THIS IS A BUG, REPORT IT HERE https://github.com/MercuryWorkshop/RecoMod"' EXIT
 }
 
 configure_binaries() {
@@ -120,28 +136,60 @@ strip_root(){
 
   >"$ROOT/usr/stripped"
 }
-shrink_image(){
+shrink_table(){
   local buffer=5000000 #5mb buffer. keeps things from breaking too much
 
-  e2fsck -f "${loopdev}p3"
-  resize2fs -M "${loopdev}p3"
-  local block_size=$(sudo tune2fs -l ${loopdev}p3 | grep -i "block size" | awk '{print $3}')
-  local block_count=$(sudo tune2fs -l ${loopdev}p3 | grep -i "block count" | awk '{print $3}')
+  supress e2fsck -fy "${loopdev}p3"
+  supress resize2fs -M "${loopdev}p3"
+  local block_size=$(tune2fs -l ${loopdev}p3 | grep -i "block size" | awk '{print $3}')
+  local sector_size=$(fdisk -l ${loopdev} | grep "Sector size" | awk '{print $4}')
+
+  local block_count=$(tune2fs -l ${loopdev}p3 | grep -i "block count" | awk '{print $3}')
   local block_count=${block_count%%[[:space:]]*}
 
   debug "bs: $block_size, blocks: $block_count"
 
   local raw_bytes=$((block_count * block_size))
-  local resize_target=$((raw_bytes + buffer))
-  debug "real size: $raw_bytes"
-  info "resizing disk to $resize_target"
+  local resized_size=$((raw_bytes + buffer))
+  local fdisk_ra_entry=$(fdisk -u -l ${loopdev} | grep ${loopdev}p3)
+  local start_sector=$(awk '{print $2}' <<<"$fdisk_ra_entry")
+  local end_sector=$(awk '{print $3}' <<<"$fdisk_ra_entry")
+  local start_bytes=$((start_sector * sector_size))
+  local end_bytes=$((end_sector * sector_size))
+  local resized_end=$((start_bytes + resized_size))
 
-  info "do the partitioning"
-  gparted "$loopdev"
+  debug "real size: $raw_bytes bytes"
+  info "resizing ${loopdev}p3 to $resized_size bytes"
+
+  debug "start of ${loopdev}p3 is $start_bytes bytes. changing end from $end_bytes bytes to $resized_end bytes"
+
+
+  # script will die if i connect a stream to /dev/null  for whatever reason, giving birth to this particular bit of jank
+  local jankfile=$(mktemp)
+  # if you're wondering why i don't use --script, it causes changes to not apply
+  # if you're wondering why i don't use <<EOF, it causes changes not to apply
+  # stupid fucking gnu devs
+  script /dev/null -c "parted ${loopdev} -f resizepart 3 ${resized_end}B" <<< "Yes" >$jankfile
+  supress cat $jankfile
+  rm -f $jankfile
+
+
+  supress sfdisk -N 1 --move-data ${loopdev} <<<"+,-"
 
 }
-main() {
+truncate_image(){
+  local buffer=1000000 #1mb buffer. keeps things from breaking too much
+  local img=$1
+  local fdisk_stateful_entry=$(fdisk -l "$img" | grep ${img}1[[:space:]])
+  local sector_size=$(fdisk -l "$img" | grep "Sector size" | awk '{print $4}')
+  local end_sector=$(awk '{print $3}' <<<"$fdisk_stateful_entry")
+  local end_bytes=$((end_sector * sector_size + buffer))
 
+  info "truncating image to $end_bytes bytes"
+
+  truncate -s $end_bytes "$img"
+}
+main() {
   if [ ! -f "$FLAGS_image" ]; then
     quit "\"$FLAGS_image\" is not a real file!!! You need to pass the path to the recovery image" 1
   fi
@@ -156,10 +204,10 @@ main() {
 
 
   if [ "$FLAGS_keep_verity" = "$FLAGS_FALSE" ]; then
-    $SSD_UTIL --remove_rootfs_verification -i "$loopdev" --partitions 4
+    supress $SSD_UTIL --remove_rootfs_verification -i "$loopdev" --partitions 4
   fi
   
-  $SSD_UTIL --remove_rootfs_verification --no_resign_kernel -i "$loopdev" --partitions 2
+  supress $SSD_UTIL --remove_rootfs_verification --no_resign_kernel -i "$loopdev" --partitions 2
 
   # for good measure
   sync
@@ -185,37 +233,59 @@ main() {
     quit "Patching successful. skipping cleanup, please do it yourself!" 0
   fi
 
-  sleep 2
+  sleep 0.2
   sync
 
-  # local duout=$(du -s "$ROOT")
-  # local minsize=${duout%%/*}
-  # local minsize=${minsize//[[:blank:]]/}
   umount "$ROOT"
 
   if [ "$FLAGS_strip" = "$FLAGS_TRUE" ]; then
-    info "Shrinking image to $minsize"
-    shrink_image "$minsize"
+    info "Shrinking GPT table"
+    shrink_table
   fi
 
   losetup -D "$loopdev"
 
-  # i know this is a bit gratuitous, but i don't want to risk the loop not unmounting and accidentally nuking the fs
+
   sync
-  sleep 2
+  sleep 0.2
+  if [ "$FLAGS_strip" = "$FLAGS_TRUE" ]; then
+    truncate_image "$bin"
+  fi
+  sync
+  sleep 0.2
 
   rm -rf "$ROOT"
   info "Patching successful, happy hacking!"
   leave 0
 }
 
+# make this sourceable for testing
 if [ "$0" = "$BASH_SOURCE" ]; then
   stty sane
+
+  traps
+  getopts "$@"
+
   if [ "$EUID" -ne 0 ]; then
     quit "Please run as root" 1
   fi
-  traps
-  getopts "$@"
+
+
+
+  if [ "$FLAGS_debug" = "$FLAGS_TRUE" ] && [ "$FLAGS_quiet" = "$FLAGS_FALSE" ]; then
+    set -x
+  fi
+
+  # breaks without this
+  if supress which sysctl; then
+    orig_sysctl=$(sysctl --values fs.protected_regular)
+    supress sysctl -w fs.protected_regular=0
+  fi
+
   configure_binaries
   main
+
+  if supress which sysctl; then
+    supress sysctl -w fs.protected_regular=$orig_sysctl
+  fi
 fi
